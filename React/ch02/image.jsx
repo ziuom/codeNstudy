28703a7600be509ee8fbdf0e357e9b5ac2461507
ziuom <kjw3301@naver.com>
2024-11-21 16:27:08 +0900

    Add files via upload

---
+++ React/ch02/image.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Image() {
+    return(
+        <div>
+            <img
+                src="https://i1.sndcdn.com/artworks-TpF8NuR2R9FyuEoK-F0OXDQ-t1080x1080.jpg"
+                style={{ width: "300px", margin: "10px" }}
+                alt="littlePrince"
+            ></img>
+            </div>
+    );
+}
+
+export default Image;
